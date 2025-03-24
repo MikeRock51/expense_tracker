80a13023b7a6eb41024e33c9759f98d8627b0cb1
Mike <mikerockmusic51@gmail.com>
2025-03-24 23:07:34 +0100

    Implement application bootstrap and initial run logic

---
+++ src/Framework/App.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Framework;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+
+class App {
+    public function run() {
+        echo 'Hello, world!';
+    }
+}
