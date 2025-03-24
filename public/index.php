af4ca3ac3655547e550fd1cccbe650b41bfd9d78
Mike <mikerockmusic51@gmail.com>
2025-03-24 22:39:18 +0100

    Rename project to expense_tracker and update autoload paths

--- public/index.php
+++ public/index.php
@@ -1,21 +1,3 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contacts</title>
-</head>
-<body>
-    <h1>Contacts</h1>
-    <ul>
-        <li><a href="contacts.php">Contacts</a></li>
-        <li><a href="contacts.php">Contacts</a></li>
-        <li><a href="contacts.php">Contacts</a></li>
-        <li><a href="contacts.php">Contacts</a></li>
-    </ul>
-</body>
-</html> -->
-
 <?php
 ini_set('memory_limit', '256M');
 echo ini_get('memory_limit');
