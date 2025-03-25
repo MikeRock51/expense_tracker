750e7f05f34aa8e0f4e3075aa0f15b166d40820d
Mike <mikerockmusic51@gmail.com>
2025-03-25 14:09:07 +0100

    Add Router class and initialize in App constructor

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -8,6 +8,12 @@ require __DIR__ . '/../../vendor/autoload.php';
 
 
 class App {
+    private Router $router;
+
+    public function __construct() {
+        $this->router = new Router();
+    }
+    
     public function run() {
         echo 'Hello, world!!!!!!!!!!';
     }
