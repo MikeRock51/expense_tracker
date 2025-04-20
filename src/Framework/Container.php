f2d9e2b8986e052b850ffffb2287e555fcd56bd1
Mike <mikerockmusic51@gmail.com>
2025-04-21 00:08:47 +0100

    feat:Implement class resolution in Container

--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -4,13 +4,18 @@ declare(strict_types=1);
 
 namespace Framework;
 
+use ReflectionClass;
+
 class Container {
     private array $definitions = [];
 
     public function addDefinitions(array $newDefinitions): void {
         $this->definitions = [...$this->definitions, ...$newDefinitions];
-        dd($this->definitions);
     }
 
-    public function resolve(string $class) {}
+    public function resolve(string $className) {
+        $reflectionClass = new ReflectionClass($className);
+
+        dd($reflectionClass);
+    }
 }
