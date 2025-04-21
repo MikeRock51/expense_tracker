332593f38a39dc2c37c4f2570d362b5aa6960313
Mike <mikerockmusic51@gmail.com>
2025-04-21 08:18:12 +0100

    feat: refactor controllers to use dependency injection

--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -49,9 +49,21 @@ class Container
             if (!$type instanceof ReflectionNamedType || $type->isBuiltin()) {
                 throw new ContainerException("Failed to resolve class {$className} because invalid param type");
             }
+
+            $dependencies[] = $this->get($type->getName());
         }
 
 
-        dd($params);
+        return $reflectionClass->newInstanceArgs($dependencies);
+    }
+
+    public function get(string $id) {
+        if (!array_key_exists($id, $this->definitions)) {
+            throw new ContainerException("Class {$id} does not exist in container");
+        }
+
+        $factory = $this->definitions[$id];
+
+        return $factory();
     }
 }
