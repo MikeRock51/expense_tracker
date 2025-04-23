bc2f1151e958b14255a265884792a9f6cedeaebb
Mike <mikerockmusic51@gmail.com>
2025-04-23 06:08:37 +0100

    feat: add RegisterController and registration view with form fields

--- src/App/Config/Routes.php
+++ src/App/Config/Routes.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 
 namespace App\Config;
 
-use App\Controllers\{HomeController, AboutController};
+use App\Controllers\{HomeController, AboutController, RegisterController};
 use Framework\App;
 
 function registerRoutes(App $app) {
     $app->add('/', [HomeController::class, 'home']);
     $app->add('/about', [AboutController::class, 'about']);
+    $app->add('/register', [RegisterController::class, 'register']);
 }
---
+++ src/App/Controllers/RegisterController.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Controllers;
+
+use Framework\TemplateEngine;
+
+class RegisterController {
+    public function __construct(private TemplateEngine $view) {}
+
+    public function register() {
+        $this->view->render('/register.php');
+    }
+}
